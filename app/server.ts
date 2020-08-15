7236cae8ac6973d7f8cbe446e16532c6d144ddd2
peter <jchoi224@my.bcit.ca>
2020-08-15 16:57:27 -0700

    swagger initial setup

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,11 @@
 // app/server.ts
 import app from "./app";
+var swaggerUi = require('swagger-ui-express'); 
+var swaggerDocument = require('./swagger.json');
+
 const PORT = 3000;
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
 })
